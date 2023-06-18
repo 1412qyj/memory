a079fa17c9e14843c695f2df4df39348256f876b
1412qyj <73373142+1412qyj@users.noreply.github.com>
2023-06-18 22:41:37 +0800

    Update memory.py

--- memory.py
+++ memory.py
@@ -3,6 +3,13 @@ import streamlit as st
 
 passwd = 'qyjwyq'
 
+url = r'memory.mp4'
+
+@st.cache_data
+def read_video():
+    with open(url, 'rb') as fp:
+        return fp.read()
+
 def show():
-    st.title('QYJ&WYQ の Memory')
-    st.video(r'memory.mp4'))
+    st.title('QYJ&WYQ1 の Memory')
+    st.video(read_video())
